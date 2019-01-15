340ec57aa3e0a57d63b07ca51870311d5ce78814
hangtenz <hang_35969@hotmail.com>
2019-01-15 19:42:12 +0700

    bug value

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -2,9 +2,13 @@ var First_Name=$('#First_Name')[0];
 var signUp=$('#SignUpForm')[0];
 var email=$('#email')[0];
 var psw=$('#psw')[0];
-var repsw = $('#psw-repeat')[0];
+var repsw = $('#repsw')[0];
 var submitBtn = $('#submitBtn');
 
 function submitForm(){
-    alert("Password Don't match");
+    console.log(psw);
+    console.log(repsw);
+    if(psw!=repsw){
+        alert("Password Don't match.");
+    }
 }
