344f2f4f6ec5664406e9e7c981e46bae1a72612c
hangtenz <hang_35969@hotmail.com>
2019-01-15 19:16:01 +0700

    fix bug

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,4 +1,10 @@
-var First_Name=$('#First_Name')
+var First_Name=$('#First_Name')[0];
+var signUp=$('#SignUpForm')[0];
+var email=$('#email')[0];
+var psw=$('#psw')[0];
+var repsw = $('#psw-repeat')[0];
+var submitBtn = $('#submitBtn');
+
 function submitForm(){
-    console.log(First_Name.value);
+    alert("Password Don't match");
 }
--- server.js
+++ server.js
@@ -1,19 +1,44 @@
 var express = require('express');
 var ejs = require('ejs');
 var app = express();
+var bodyParser = require('body-parser')
 
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+app.post('/signUp',function(req,res){
+    var first_name = req.body.first_name;
+    var last_name = req.body.last_name;
+    var email = req.body.email;
+    var psw = req.body.psw;
+    var repsw = req.body.repsw;
+
+    var success = true;
+    if(psw!=repsw){
+        success=false;
+    }
+    if(success){
+        res.redirect('/');
+    }
+    else{
+        res.redirect('signUp');
+    }
+});
 
 app.get('/signUp',function(req,res){
     res.render('signUp');
 });
-app.get('js/jQuery.js',function(req,res){
-    res.sendfile("/js/jQurey.js");
-});
+
 app.get('/', function(req, res) {
     res.render('index');
 });
+app.post('/',function(req,res){
+    alert("ERROR");
+});
+
 app.listen(3000,function(){
     console.log("listening on port 3000");
 });
